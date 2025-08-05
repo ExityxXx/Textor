1167b282790e1f118b602d07133f87ba35677fe2
Exityx <gdlasorum@gmail.com>
2025-08-05 17:07:50 +0300

    Modified main.cpp (added logic)

---
+++ src/main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <cstdlib>
+
+int main(void)
+{
+    std::string line;
+    std::ofstream file;
+
+    std::cout << "\t\t   Small CLI redactor for C++\n\t\t      for quit enter ':q'.\n\n";
+    std::cout << "Enter file name to save all content (without extension): ";
+    
+    std::string file_name;
+    (std::cin >> file_name).get();
+
+    std::cout << "Enter file name extension: ";
+    std::string file_name_extension;
+    (std::cin >> file_name_extension).get();
+
+    file.open(file_name + file_name_extension);
+    system("cls");
+
+    std::cout << "Edit: " << file_name + file_name_extension << "\n\n";
+    int i = 1;
+    std::cout << i << " | ";
+    
+    while (std::getline(std::cin, line))
+    {
+        if (line == ":q") break;
+        std::cout << ++i << " | ";
+        file << line << std::endl;
+    }
+    
+    file.close();
+    std::cout << "\nAll code saved in file '" << file_name + file_name_extension << "'.\n";
+    std::cout << "Compile this code? (Y/n): ";
+
+    char ch;
+    std::cin.clear();
+    std::cin >> ch;
+
+    if (ch == 'Y' || ch == 'y')
+    {
+        if (file_name_extension == ".cpp")
+        {
+            std::string compile_command = "cls & g++ " + file_name + file_name_extension + " -o " + file_name + " & " + file_name;
+            system(compile_command.c_str());
+        }
+
+        else std::cout << "Undefined extension name.\n";
+    }
+
+    else if (ch == 'N' || ch == 'n')
+        std::cout << "Bye!\n";
+    else
+        std::cout << "Undefined character.\n";
+        
+    return 0;
+}
